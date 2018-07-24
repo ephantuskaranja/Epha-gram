2848a40a32cc6b6d39f2b10fa0f979d1c6fcb88f
Ephantus karanja <ephantusslogan@gmail.com>
2018-07-24 11:59:07 +0300

    add profile model

--- my_gram/models.py
+++ my_gram/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='profileimages/')
+    bio = models.TextField(max_length=100)
+    user = models.ForeignKey(User)
+    gram_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.gram_name
+
