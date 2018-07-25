a6ef42b31f0104f814132c57502a361df83bbdcd
Ephantus karanja <ephantusslogan@gmail.com>
2018-07-25 12:16:01 +0300

    add upload pictures form

--- my_gram/views.py
+++ my_gram/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-from my_gram.forms import ProfileForm
+from my_gram.forms import ProfileForm, UploadPicForm
 
 
 @login_required(login_url='/accounts/login/')
