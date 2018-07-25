62410377c3fe6837136974f8ed388d46d6666837
Ephantus karanja <ephantusslogan@gmail.com>
2018-07-25 11:37:33 +0300

    add upload picture views

--- my_gram/models.py
+++ my_gram/models.py
@@ -8,8 +8,12 @@ class Profile(models.Model):
     profile_photo = models.ImageField(upload_to='profileimages/')
     bio = models.TextField(max_length=100)
     user = models.ForeignKey(User)
-    gram_name = models.CharField(max_length=30)
+
 
     def __str__(self):
         return self.gram_name
 
+class Pictures(models.Model):
+    picture = models.ImageField(upload_to='profileimages/')
+    user = models.ForeignKey(User)
+    gram_name = models.CharField(max_length=30)
--- my_gram/views.py
+++ my_gram/views.py
@@ -22,3 +22,15 @@ def new_profile(request):
     else:
         form = ProfileForm()
     return render(request, 'new_profile.html', {"form": form})
+
+def upload_pic(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UploadPicForm(request.POST, request.FILES)
+        if form.is_valid():
+            pic = form.save(commit=False)
+            profile.user = current_user
+            pic.save()
+    else:
+        form = UploadPicForm()
+    return render(request, 'UploadPicForm.html', {"form": form})
