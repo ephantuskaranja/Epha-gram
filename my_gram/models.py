662a0092aced2e1886d5934837d223f5f4f3b2f7
Ephantus karanja <ephantusslogan@gmail.com>
2018-07-29 11:31:31 +0300

    create comment model

--- my_gram/models.py
+++ my_gram/models.py
@@ -27,3 +27,8 @@ class Pictures(models.Model):
 
     def __str__(self):
         return self.gram_name
+
+class Comment(models.Model):
+    user = models.ForeignKey(User)
+    picture = models.ForeignKey(Pictures)
+    comment_text = models.CharField(max_length=100)
