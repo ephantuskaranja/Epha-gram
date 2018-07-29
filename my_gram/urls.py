8dc05082cc6580991365e5d02117c88263b451ce
Ephantus karanja <ephantusslogan@gmail.com>
2018-07-29 11:54:32 +0300

    add comments url

--- my_gram/urls.py
+++ my_gram/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    url(r'^comment/(?P<picture_id>\d+)', views.addcomment, name='comment'),
 
 
 ]
