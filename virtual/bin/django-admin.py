2ba20199085590bdd523b37cfa63fce3a388b8e2
Ephantus karanja <ephantusslogan@gmail.com>
2018-08-07 13:06:36 +0300

    return deleted folder the_gram

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ephantus/Desktop/Moringa_school_projects/Core-Projects/Django/the-gram/the_gram/virtual/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
